461df19c1e0abd6f10310981f6acfa9e5b46301f
lzwang <zhuangwang82@gmail.com>
2022-04-20 23:22:56 +0800

    update: logger format
    Change-Id: Ic28c91a5cb5c13bdf379f75a3a72dc4d8a6148cb

--- src/logger.py
+++ src/logger.py
@@ -4,29 +4,29 @@ import sys
 from loguru import logger
 
 
-def add_log_to_console():
+def add_log_to_console(log_level: str = 'INFO'):
     logger.add(
         sink=sys.stderr,
-        level='INFO',
+        level=log_level,
         format='<green>{time:MM-DD HH:mm:ss.SSS}</green> '
                '| <level>{level: <8}</level> '
-               '| <cyan>{name: <30}</cyan>: <cyan>{function: <18}'
-               '</cyan>: <cyan>line.{line: <4}</cyan> | '
+               '| <cyan>{name:}</cyan>: <cyan>{function:} '
+               '</cyan>: <cyan>line.{line:}</cyan> | '
                '<level>{message}</level> '
     )
 
 
-def add_log_to_file(log_dir: str):
+def add_log_to_file(log_dir: str, log_level: str = 'DEBUG'):
     logger.add(
         sink=os.path.join(log_dir, 'log_{time:YYYY_MM}.log'),
-        level='DEBUG',
+        level=log_level,
         format="{time:YYYY-MM-DD HH:mm:ss.SSS} "
-               "| {level: <8} | {name:} -> {function:} -> line.{line} | {message}",
-        rotation='5 MB',  # create a new log file when log file size > 5 MB
-        retention='3 months',  # keep log file time: "1 week, 3 days"、"2 months"
-        backtrace=True,
+               "| {level: <8} | {name:}:{function:}:line.{line} | {message}",
+        rotation='10 MB',  # create a new log file when log file size > 5 MB
+        retention='1 months',  # keep log file time: "1 week, 3 days"、"2 months"
+        # backtrace=True,
         encoding="utf-8",
-        compression='zip'  # zip、tar、gz、tar.gz
+        # compression='zip'  # zip、tar、gz、tar.gz
     )
 
 
@@ -42,17 +42,19 @@ def show_log_msgs():
     logger.warning('*' * 50)
 
 
-def init_logger(log_dir: str = None):
+def init_logger(log_dir: str = None,
+                console_log_level: str = "INFO",
+                file_log_level: str = "DEBUG"):
     """loguru全局初始化，不需要再次导入"""
     # 移除掉自带的sink
     logger.remove()
 
     # to console
-    add_log_to_console()
+    add_log_to_console(console_log_level)
 
     # to file
     if log_dir is not None:
-        add_log_to_file(log_dir)
+        add_log_to_file(log_dir, file_log_level)
 
     # simple test log output
     show_log_msgs()
