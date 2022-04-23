03082dfdfe1487bf19520437047617e4b89053c4
lzwang <zhuangwang82@gmail.com>
2022-04-23 13:13:30 +0800

    update: optimise shell output
    Change-Id: Ib1681351cad234cb19cc2145cf1be16f0ec89cea

--- src/sh.py
+++ src/sh.py
@@ -7,8 +7,8 @@ def run_cmd(cmd: list):
     log.info(f"[run shell cmd] {' '.join(cmd)}")
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    out = stdout.decode("utf-8")
-    err = stderr.decode("utf-8")
+    out = stdout.decode("utf-8").strip()
+    err = stderr.decode("utf-8").strip()
     if err:
         log.error(f'[run shell cmd] Failed, detail: \n{err}')
     if out:
