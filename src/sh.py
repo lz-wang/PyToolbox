a6e1294efd0d868ddcad277d4c4b2044a3e11d19
lzwang <zhuangwang82@gmail.com>
2022-10-26 21:47:11 +0800

    cmd支持隐藏指令输入输出
    Change-Id: I85a648ad1cb627196fcd6e961634efc097db6ea8

--- src/sh.py
+++ src/sh.py
@@ -2,15 +2,17 @@ import subprocess
 from loguru import logger as log
 
 
-def run_cmd(cmd: list):
+def run_cmd(cmd: list, show_cmd: bool = True, show_results: bool = True):
     """Example: cmd=['echo', 'hello world']"""
-    log.info(f"[run shell cmd] {' '.join(cmd)}")
+    if show_cmd:
+        log.info(f"[run shell cmd] {' '.join(cmd)}")
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     out = stdout.decode("utf-8").strip()
     err = stderr.decode("utf-8").strip()
-    if err:
-        log.warning(f'[run shell cmd] STD_ERR: \n{err}')
-    if out:
-        log.success(f'[run shell cmd] STD_OUT, output: \n{out}')
+    if show_results:
+        if err:
+            log.warning(f'[run shell cmd] STD_ERR: \n{err}')
+        if out:
+            log.success(f'[run shell cmd] STD_OUT, output: \n{out}')
     return out, err
